1c49d2ae4920b21fdac37160bf98daaeece8037d
mostafashahin <mostafa_shahin@ieee.org>
2020-12-15 13:19:55 +1100

    Required models

---
+++ g2p/phonetisaurus/get_dict.sh
@@ -0,0 +1,33 @@
+#!/bin/env bash
+IFS=$'\n'
+
+wget https://raw.githubusercontent.com/cmusphinx/cmudict/master/cmudict.dict
+cat cmudict.dict \
+  | perl -pe 's/\([0-9]+\)//;
+              s/[0-9]//g;
+              /#/d;
+              s/\s+/ /g; s/^\s+//;
+              s/\s+$//; @_ = split (/\s+/);
+              $w = shift (@_);
+              $_ = $w."\t".join (" ", @_)."\n";' | tr [:lower:] [:upper:] > cmudict.formatted_upp.dict
+
+cmu_dict=cmudict.formatted_upp.dict
+arpa2aus_dict=data/arpa2aus.dict
+g2arpa_model=data/model_g2arpa_8.fst
+arpa2aus_model=data/model_arpa2aus_8.fst
+
+in_wrd_lst=$1
+out_dict=$2
+
+mkdir -p tmp
+
+#get arpa phoneme trans
+phonetisaurus-apply --model $g2arpa_model --word_list $in_wrd_lst -l $cmu_dict > tmp/arpa_gen.dict
+
+#Get aus trans from arpa using p2p model and eval
+cat tmp/arpa_gen.dict | cut -f2 > tmp/arpa_gen.trans
+cat tmp/arpa_gen.dict | cut -f1 > tmp/arpa_gen.wrds
+phonetisaurus-apply -gs " " --model $arpa2aus_model --word_list tmp/arpa_gen.trans > tmp/arpa_aus_gen.dict
+cat tmp/arpa_aus_gen.dict | while read line; do t=`echo $line | cut -f2`; l=`echo $line | cut -f1`; m=`grep -P "\t$l$" tmp/arpa_gen.dict | cut -f1` ; for w in $m; do echo -e "$w\t$t"; done; done > tmp/aus_gen.dict
+
+cp tmp/aus_gen.dict $out_dict
